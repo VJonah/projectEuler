ca718c08a328a651d35e894104c779851d97e283
Jonah <jonahvairon@hotmail.co.uk>
2021-04-26 14:41:40 +0100

    Problem 18 solved with a not so great solution.

--- p18/main.go
+++ p18/main.go
@@ -8,9 +8,66 @@ import (
 )
 
 func main() {
-	input := parseInput("test.txt")
-	fmt.Println(input)
-	//startNode := &Node{Value: input[0][0]}
+	input := parseInput("triangles.txt")
+	root := generateTree(input[1:], &Node{Value: input[0]}, 2)
+	fmt.Println(root)
+	//values := &[]int{}
+	//getValues(root, values)
+	//fmt.Println(findLargest(*values))
+	//fmt.Printf("=%d\n", traverseForMaximum(root))
+}
+
+func getValues(node *Node, values *[]int) {
+	if node != nil {
+		*values = append(*values, node.Value)
+		getValues(node.Left, values)
+		getValues(node.Right, values)
+	}
+}
+
+func findLargest(values []int) int {
+	largest := 0
+	for _, x := range values {
+		if x > largest {
+			largest = x
+		}
+	}
+	return largest
+}
+
+func generateTree(pyramid []int, curNode *Node, numberOfNodes int) *Node {
+	if len(pyramid) == 0 {
+		return nil
+	}
+	left := &Node{Value: pyramid[0] + curNode.Value}
+	right := &Node{Value: pyramid[1] + curNode.Value}
+	curNode.Left = left
+	curNode.Right = right
+	if len(pyramid) <= numberOfNodes+1 {
+		generateTree([]int{}, left, numberOfNodes+1)
+	} else {
+		generateTree(pyramid[numberOfNodes:], left, numberOfNodes+1)
+	}
+	if len(pyramid) <= numberOfNodes+2 {
+		generateTree([]int{}, right, numberOfNodes+1)
+	} else {
+		generateTree(pyramid[numberOfNodes+1:], right, numberOfNodes+1)
+	}
+	return curNode
+}
+
+func traverseForMaximum(node *Node) int {
+	fmt.Printf("%d+\n", node.Value)
+	if node.Left == nil {
+		return node.Value
+	}
+	if node.Left.Value == node.Right.Value {
+		fmt.Println("Equals!")
+	}
+	if node.Left.Value > node.Right.Value {
+		return node.Value + traverseForMaximum(node.Left)
+	}
+	return node.Value + traverseForMaximum(node.Right)
 }
 
 type Node struct {
@@ -19,43 +76,24 @@ type Node struct {
 	Value int
 }
 
-func (n *Node) String() string {
-	fmt.Println(n.Value)
-	if n.Left != nil {
-		fmt.Println(n.Left.String())
+func (n *Node) String() {
+	if n != nil {
+		fmt.Println(n.Value)
+		n.Left.String()
+		n.Right.String()
 	}
-	if n.Right != nil {
-		fmt.Println(n.Right.String())
-	}
-	s := strconv.Itoa(n.Value)
-	return s
 }
 
-func findLargest(node *Node, largest int) int {
-	fmt.Println(largest, node.Value)
-	if node.Left == nil {
-		if largest < node.Value {
-			largest = node.Value
-		}
-		return largest
-	}
-	findLargest(node.Left, largest)
-	findLargest(node.Right, largest)
-	return largest
-}
-
-func parseInput(href string) [][]int {
+func parseInput(href string) []int {
 	data, _ := ioutil.ReadFile(href)
 	lines := strings.Split(string(data), "\n")
-	pyramid := [][]int{}
+	pyramid := []int{}
 	for _, line := range lines {
 		numbers := strings.Split(line, " ")
-		row := []int{}
 		for _, n := range numbers {
 			val, _ := strconv.Atoi(n)
-			row = append(row, val)
+			pyramid = append(pyramid, val)
 		}
-		pyramid = append(pyramid, row)
 	}
 	return pyramid
 }
